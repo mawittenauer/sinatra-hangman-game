bd945d05421ccdb3ffb4912e2dfbfbfe1da14475
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2015-10-28 14:37:39 -0400

    Initialize Repository

---
+++ main.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'sinatra'
+
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'your_secret' 
+
+
+
+
